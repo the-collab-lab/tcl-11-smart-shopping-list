5d5f000895260680dd7f5013ecee98ecfc2d3ac6
Red <vhanchar@ucsc.edu>
2020-08-10 16:20:02 -0700

    token generation

--- src/components/List/List.component.jsx
+++ src/components/List/List.component.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import getToken from '../../lib/tokens';
 
-const List = () => (
-  <div>
-    <h1 className="page__title">List</h1>
-  </div>
-);
+const List = () => {
+  //Should be a button/link to create a new list
+
+  //To generate a new token:
+  const token = getToken();
+  console.log('local storage: ', localStorage);
+
+  //To set the item to the local storage
+  localStorage.setItem('token', token);
+  console.log('local storage: ', localStorage);
+  // localStorage.removeItem('token');
+
+  //Check if there is a saved token in the localStorage
+  const localToken = localStorage.getItem('token');
+
+  // create a token if doesn't exist
+  if (localToken === null) {
+    console.log('no token');
+  }
+
+  return (
+    <div>
+      <h1 className="page__title">List</h1>
+      Should be token: {token}
+    </div>
+  );
+};
 
 export default List;
