ea2ab082e7781e1578148445e80f865cba8044a5
CodeMeKathy * <Kathy@KathyLambert.me>
2020-08-11 13:03:27 -0400

    Refactor: Add <Home/> and refractor routing
    - Combined <AddItem/> and <AddItemFirebase/> components
    - Add a <Home /> component
    - Add a <Home /> component route with <List /> component new route
    - Add wireframe images
    
    Co-Authored-By: Volha Hancharova <44907238+daredtech@users.noreply.github.com>

--- src/components/AddItem/AddItem.component.jsx
+++ src/components/AddItem/AddItem.component.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as firebase from '../Firebase/Firebase.component';
 
-const AddItem = () => {
+const AddItem = ({ newItemValue }) => {
+  const [itemName, setItemName] = useState('');
+  const collectionName = 'items';
+  const randomString = require('randomstring');
+  const id = randomString.generate(20);
+
+  //To update the value on change
+  const handleChanges = event => {
+    setItemName(event.target.value);
+  };
+
+  //To add the item to the database
+  const addNewItemValue = event => {
+    firebase.dataBase
+      .collection(collectionName)
+      .doc(id)
+      .set({ name: itemName });
+    setItemName('');
+  };
+
   return (
-    <div>
+    <div className="classNewItem">
       <h1 className="page__title">Add Item</h1>
+      <input
+        id="itemId"
+        value={itemName}
+        name="name"
+        onChange={handleChanges}
+      />
+
+      <button onClick={addNewItemValue}> Add Item</button>
     </div>
   );
 };
--- src/components/component.index.js
+++ src/components/component.index.js
@@ -1,5 +1,6 @@
+import Home from './Home/Home.component.jsx';
 import AddItem from './AddItem/AddItem.component';
 import List from './List/List.component';
 import Footer from './Footer/Footer.component.jsx';
 
-export { AddItem, List, Footer };
+export { Home, AddItem, List, Footer };
