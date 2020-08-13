b7776a1e16cb8c41d1f97071e7b9e761abfbf680
Brandon Lee <brandonlee2543@hotmail.com>
2020-08-12 23:13:51 -0400

    Added data points to shopping list to record purchases

--- src/components/AddItem/AddItem.component.jsx
+++ src/components/AddItem/AddItem.component.jsx
@@ -1,41 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as firebase from '../Firebase/Firebase.component';
+import randomString from 'randomstring';
+
+import { FormInput, FormRadioButtons } from '../component.index';
+
+import './AddItem.style.scss';
 
 const AddItem = ({ newItemValue }) => {
-  const [itemName, setItemName] = useState('');
+  const [itemName, setItemName] = useState(null);
+  const [resupplyPeriod, setResupplyPeriod] = useState(7);
+  const [lastPurchaseDate, setLastPurchaseDate] = useState(null);
   const collectionTokenName = localStorage.getItem('token');
-  const randomString = require('randomstring');
   const itemId = randomString.generate(20);
 
   //To update the value on change
-  const handleChanges = event => {
+  const handleChange = event => {
     setItemName(event.target.value);
+  };
+
+  const handleRadioChange = event => {
+    setResupplyPeriod(event.target.value);
   };
 
   //To add the item to the database
   const addNewItemValue = event => {
+    event.preventDefault();
     firebase.dataBase
       .collection(collectionTokenName)
       .doc(itemId)
-      .set({ name: itemName, id: itemId });
+      .set({
+        name: itemName,
+        resupplyPeriod: resupplyPeriod,
+        id: itemId,
+        lastPurchaseDate: lastPurchaseDate,
+      });
     setItemName('');
   };
+
+  useEffect(() => {
+    console.log(`itemName: ${itemName}`);
+  }, [itemName]);
+
+  useEffect(() => {
+    console.log(`resupplyPeriod: ${resupplyPeriod}`);
+  }, [resupplyPeriod]);
 
   return (
     <div className="classNewItem">
       <h1 className="page__title">Add Item</h1>
-      <form onClick={addNewItemValue}>
-        <label htmlFor="itemName">Item Name: </label>
+      <form onSubmit={addNewItemValue} className="item__form">
+        {/* <label htmlFor="itemName">Item Name: </label>
         <input
           id="itemId"
           type="text"
           value={itemName}
           name="name"
           onChange={handleChanges}
-        />
-        <br />
-        <br />
-        <button type="submit"> Add Item</button>
+        /> */}
+        <div className="item__form__wrapper">
+          <FormInput
+            label={'Item Name'}
+            value={itemName}
+            onChange={handleChange}
+          />
+          <FormRadioButtons handleRadioChange={handleRadioChange}>
+            How soon will you buy this again?
+          </FormRadioButtons>
+          <br />
+          <br />
+          <button type="submit"> Add Item</button>
+        </div>
       </form>
     </div>
   );
---
+++ src/components/FormInput/FormInput.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './FormInput.style.scss';
+
+const FormInput = ({ htmlFor, label, ...otherProps }) => (
+  <div className="form__group">
+    <label htmlFor={htmlFor} className="form__label">
+      {label}:
+    </label>
+    <input type="text" {...otherProps} required className="form__input" />
+  </div>
+);
+
+export default FormInput;
