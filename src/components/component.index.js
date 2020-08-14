d97b3ab9db61303144c638dd733cdfa5d8e60eb3
Brandon Lee <brandonlee2543@hotmail.com>
2020-08-13 23:14:59 -0400

    Added new items to component's index file for easy and neat exports.
    Co-Authored-By: Stefanie Davis  <54756529+stef-codes@users.noreply.github.com>

--- src/components/component.index.js
+++ src/components/component.index.js
@@ -1,8 +1,17 @@
-import Home from './Home/Home.component.jsx';
 import AddItem from './AddItem/AddItem.component';
+import CustomButton from './CustomButton/CustomButton.component';
+import Home from './Home/Home.component.jsx';
 import List from './List/List.component';
 import Footer from './Footer/Footer.component.jsx';
 import FormInput from './FormInput/FormInput.component';
 import FormRadioButtons from './FormRadioButtons/FormRadioButtons.component';
 
-export { Home, AddItem, List, Footer, FormInput, FormRadioButtons };
+export {
+  AddItem,
+  CustomButton,
+  Home,
+  List,
+  Footer,
+  FormInput,
+  FormRadioButtons,
+};
