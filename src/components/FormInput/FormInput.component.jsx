1f1eb54639c2a33f27d57ab5bd53a2bfd5809891
Brandon Lee <brandonlee2543@hotmail.com>
2020-08-13 23:13:46 -0400

    Added Styling for our Form's item input
    Co-Authored-By: Stefanie Davis  <54756529+stef-codes@users.noreply.github.com>

--- src/components/FormInput/FormInput.component.jsx
+++ src/components/FormInput/FormInput.component.jsx
@@ -4,7 +4,7 @@ import './FormInput.style.scss';
 
 const FormInput = ({ htmlFor, label, ...otherProps }) => (
   <div className="form__group">
-    <label htmlFor={htmlFor} className="form__label">
+    <label htmlFor={htmlFor} className="item__label">
       {label}:
     </label>
     <input type="text" {...otherProps} required className="form__input" />
