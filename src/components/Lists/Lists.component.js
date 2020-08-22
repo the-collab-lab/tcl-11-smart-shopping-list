461363daac890a5261f32802a8596f7ef2b2c583
Brandon Lee <brandonlee2543@hotmail.com>
2020-08-22 18:58:51 -0400

    Adjusted styling of Lists component
    Co-Authored-By: Volha Hancharova <44907238+daredtech@users.noreply.github.com>

--- src/components/Lists/Lists.component.js
+++ src/components/Lists/Lists.component.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Lists.style.scss';
+
 const Lists = () => {
   const keys = Object.keys(localStorage);
 
   return (
     <div>
-      <p>AVAILABLE LISTS:</p>
+      <p className="lists__header">Available Lists:</p>
 
       {keys.map(key => (
-        <Link to={{ pathname: '/list', state: { localToken: key } }}>
+        <Link
+          className="lists__link"
+          to={{ pathname: '/list', state: { localToken: key } }}
+        >
           <div> {key} </div>
         </Link>
       ))}
