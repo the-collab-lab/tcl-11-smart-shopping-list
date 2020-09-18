edcc25467648bd41af35d8622c99356fb942c468
Brandon Lee <brandonlee2543@hotmail.com>
2020-09-18 15:55:19 -0400

    Added Item dropdown functionality to show details
    - Used Accordion component to provide materialUI support
    - Currently shows  `lastPurchaseDate` field
    
    Co-Authored-By: Kathy Lambert <32943879+CodeMeKathy@users.noreply.github.com>

--- src/components/Item/Item.component.jsx
+++ src/components/Item/Item.component.jsx
@@ -1,22 +1,47 @@
-import React, { useEffect } from 'react';
-import * as firebase from '../../lib/firebase';
+// React Imports
+import React, { useState, useEffect } from 'react';
+
+// Custom Imports
 import calculateEstimate from '../../lib/estimates';
-import Checkbox from '@material-ui/core/Checkbox';
+import * as firebase from '../../lib/firebase';
 import './Item.style.scss';
 
+// Material-UI Imports
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Checkbox from '@material-ui/core/Checkbox';
+
 const Item = props => {
-  const itemName = props.name;
   const itemId = props.id.toString();
+  const secondsInDay = 86400;
+  let lastPurchasedTimeInSeconds = 0;
+  const itemName = props.name;
   const localToken = props.localToken;
   const over24 = props.over24;
   const resupplyPeriod = props.resupplyPeriod;
-  const secondsInDay = 86400;
   let lastEstimate = props.lastEstimate;
   let latestInterval = props.latestInterval;
   let numberOfPurchases = props.numberOfPurchases;
   let nextPurchaseInterval = props.nextPurchaseInterval;
   let lastPurchaseDate = props.date;
-  let lastPurchasedTimeInSeconds = 0;
+
+  // Future implementation;  currently breaks but that's a low-priority issue.
+
+  // const { itemName, localToken, over24, resupplyPeriod } = props;
+  // let {
+  //   lastEstimate,
+  //   latestInterval,
+  //   numberOfPurchases,
+  //   nextPurchaseInterval,
+  //   lastPurchaseDate,
+  // } = props;
+
+  const [dropdownLastPurchaseDate, setDropdownLastPurchaseDate] = useState(
+    null,
+  );
 
   // To update the purchase date
   const markPurchased = event => {
@@ -44,6 +69,15 @@ const Item = props => {
       latestInterval,
       numberOfPurchases,
     );
+
+    // Sets date formatting for our dropdown view
+
+    let day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const parsedDate = `${month}/${day}/${year}`;
+
+    setDropdownLastPurchaseDate(parsedDate);
 
     firebase.dataBase
       .collection(localToken)
@@ -97,18 +131,29 @@ const Item = props => {
   });
 
   return (
-    <div
+    <Accordion
       id={itemId}
       onClick={markPurchased}
       aria-label={`Estimated number of days till next next purchase: ${nextPurchaseInterval}`}
     >
-      <Checkbox
-        checked={!over24}
-        value="checkedItem"
-        inputProps={{ 'aria-label': 'Checkbox Item' }}
-      />
-      {itemName}
-    </div>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Checkbox
+          checked={!over24}
+          value="checkedItem"
+          inputProps={{ 'aria-label': 'Checkbox Item' }}
+        />
+        <Typography>{itemName}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {/* lastPurchaseDate, Estimated number of days until purchase is made, Number of times items  has been purchased*/}
+
+        {dropdownLastPurchaseDate}
+      </AccordionDetails>
+    </Accordion>
   );
 };
 
