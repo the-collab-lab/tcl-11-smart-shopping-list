a3378b4ad52ab73aa1f577a3c620a79bf28f87ac
CodeMeKathy * <Kathy@KathyLambert.me>
2020-09-23 15:14:42 -0400

    Refactor(<List/>): Add Material-UI <Grid/> with styling
    Co-Authored-By: Volha Hancharova <44907238+daredtech@users.noreply.github.com>

--- src/components/List/List.component.jsx
+++ src/components/List/List.component.jsx
@@ -1,11 +1,40 @@
+// React Imports
 import React, { useState, useEffect } from 'react';
-import Listener from '../../services/Listener/Listener.service';
-
-import { Footer } from '../component.index';
 import { useHistory } from 'react-router-dom';
+
+// Material-UI Imports
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+// Custom Imports
+import Listener from '../../services/Listener/Listener.service';
+import { Footer } from '../component.index';
 import { CustomButton } from '../component.index';
+import './List.style.scss';
 
 const List = props => {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      height: '100vh',
+      backgroundImage:
+        'url(https://images.unsplash.com/photo-1571211905393-6de67ff8fb61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      alignItems: 'center',
+    },
+    list: {
+      backgroundColor: 'yellow',
+      width: '50vw',
+      height: '50vh',
+      alignItems: 'center',
+      margin: '0 auto',
+      direction: 'column',
+    },
+    page: {
+      margin: '0 auto',
+    },
+  }));
+
   const history = useHistory();
   const listProps = props.location.state;
   const token = listProps.localToken;
@@ -20,27 +49,33 @@ const List = props => {
     setLocalToken(token);
   }, []);
 
+  const classes = useStyles();
+
   return (
-    <div className="list__page">
-      <h1 className="page__title">Shopping List</h1>
+    <Grid container component="list" className={classes.root} id="list__grid">
+      <Grid container component="list" className={classes.list}>
+        <div className={classes.page}>
+          <h1 className="page__title">Shopping List</h1>
 
-      {localToken ? (
-        <>
-          <Listener localToken={localToken} />
+          {localToken ? (
+            <>
+              <Listener localToken={localToken} />
 
-          <button onClick={redirectAddItem}> Add Item</button>
-        </>
-      ) : (
-        <>
-          <div>
-            <p> Your shopping list is currently empty.</p>
-            <CustomButton onClick={redirectAddItem}>Add Item</CustomButton>
-          </div>
-        </>
-      )}
-      <> </>
+              <button onClick={redirectAddItem}> Add Item</button>
+            </>
+          ) : (
+            <>
+              <div>
+                <p> Your shopping list is currently empty.</p>
+                <CustomButton onClick={redirectAddItem}>Add Item</CustomButton>
+              </div>
+            </>
+          )}
+          <> </>
+        </div>
+      </Grid>
       <Footer />
-    </div>
+    </Grid>
   );
 };
 
